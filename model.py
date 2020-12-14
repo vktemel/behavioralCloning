9e7e08f194f601dcbfae252ab47d15b51ba40a9f
vktemel <vktemel@umich.edu>
2020-12-13 20:11:33 -0500

    added a more powerful network

--- model.py
+++ model.py
@@ -36,11 +36,25 @@ for datapoint in range(df_log.shape[0]):
 X_train = np.array(images)
 y_train = np.array(measurements)
 
-from keras.models import Sequential
-from keras.layers import Flatten, Dense
+from keras.models import Model, Sequential
+from keras.layers import Flatten, Dense, Lambda, Cropping2D
+from keras.layers.convolutional import Convolution2D
+from keras.layers.pooling import MaxPooling2D
+import matplotlib.pyplot as plt
 
 model = Sequential()
-model.add(Flatten(input_shape = (160,320,3)))
+model.add(Lambda(lambda x: ((x/255.0) - 0.5), input_shape=(160,320,3)))
+model.add(Cropping2D(cropping=((50,20),(0,0))))
+model.add(Convolution2D(24, kernel_size=(5,5), strides=(2,2), activation='relu'))
+model.add(Convolution2D(36, kernel_size=(5,5), strides=(2,2), activation='relu'))
+model.add(Convolution2D(48, kernel_size=(5,5), strides=(2,2), activation='relu'))
+model.add(Convolution2D(64, kernel_size=(3,3), activation='relu'))
+model.add(Convolution2D(64, kernel_size=(3,3), activation='relu'))
+model.add(Flatten())
+model.add(Dense(1164))
+model.add(Dense(100))
+model.add(Dense(50))
+model.add(Dense(10))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
