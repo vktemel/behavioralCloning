c660382d7a8974260c131465567d16a1ea8a16c6
vktemel <vktemel@umich.edu>
2020-12-19 23:55:47 -0500

    added flipped images to the training and validation set in the generator

--- model.py
+++ model.py
@@ -7,50 +7,37 @@ import matplotlib.pyplot as plt
 import sklearn
 from math import ceil
 from sklearn.model_selection import train_test_split
+import random
 
 os.environ['TF_FORCE_GPU_ALLOW_GROWTH'] = 'true'
 
-# def generator(samples, batch_size=32):
-#     num_samples = len(samples)
-#     while 1: # Loop forever so the generator never terminates
-#         sklearn.utils.shuffle(samples)
-#         for offset in range(0, num_samples, batch_size):
-#             batch_samples = samples[offset:offset+batch_size]
+def generator(samples, batch_size=32):
+    num_samples = len(samples)
+    while 1: # Loop forever so the generator never terminates
+        sklearn.utils.shuffle(samples)
+        for offset in range(0, num_samples, batch_size):
+            batch_samples = samples[offset:offset+batch_size]
 
-#             images = []
-#             measurements = []
-#             for datapoint in range(batch_samples.shape[0]):
-#                 imgName = df_log.Center_Image[datapoint].split('\\')[-1]
-#                 imgPath = 'data_old\\IMG\\' + imgName
-#                 images.append(cv2.imread(imgPath))
-#                 measurements.append(df_log.Steering[datapoint])
+            images = []
+            measurements = []
+            for batch_sample in range(len(batch_samples)):
+                imgName = batch_samples.iloc[batch_sample].img_path.split('\\')[-1]
+                imgPath = data_folder + '\\IMG\\' + imgName
+                img = cv2.imread(imgPath)
+                meas = batch_samples.iloc[batch_sample].steering
+                images.append(cv2.flip(img,1))
+                measurements.append(meas*-1.0)
+                images.append(img)
+                measurements.append(meas)
 
-#                 correction = 0.2
-#                 imgName = df_log.Left_Image[datapoint].split('\\')[-1]
-#                 imgPath = 'data_old\\IMG\\' + imgName
-#                 images.append(cv2.imread(imgPath))
-#                 measurements.append(df_log.Steering[datapoint] + correction)
+            X_train = np.array(images)
+            y_train = np.array(measurements)
+            X_train, y_train = sklearn.utils.shuffle(X_train, y_train)
+            yield X_train, y_train
 
-#                 imgName = df_log.Right_Image[datapoint].split('\\')[-1]
-#                 imgPath = 'data_old\\IMG\\' + imgName
-#                 images.append(cv2.imread(imgPath))
-#                 measurements.append(df_log.Steering[datapoint]-correction)
-
-#             augmented_images, augmented_measurements = [], []
-#             for image,measurement in zip(images,measurements):
-#                 augmented_images.append(image)
-#                 augmented_measurements.append(measurement)
-#                 augmented_images.append(cv2.flip(image,1))
-#                 augmented_measurements.append(measurement*-1.0)
-
-#             # trim image to only see section with road
-#             X_train = np.array(augmented_images)
-#             y_train = np.array(augmented_measurements)
-#             X_train, y_train = sklearn.utils.shuffle(X_train, y_train)
-#             yield X_train, y_train
-
-drive_log = 'data/driving_log.csv'
-df_log = pd.read_csv(drive_log, names=['Center_Image', 'Left_Image', 'Right_Image', 'Steering', 'Throttle', 'Brake', 'Speed'])
+data_folder = 'data_old'
+log_path = data_folder + '/driving_log.csv'
+df_log = pd.read_csv(log_path, names=['Center_Image', 'Left_Image', 'Right_Image', 'Steering', 'Throttle', 'Brake', 'Speed'])
 
 df_log.Center_Image = df_log.Center_Image.apply(lambda x: x.split('\\')[-1])
 df_log.Left_Image = df_log.Left_Image.apply(lambda x: x.split('\\')[-1])
@@ -67,35 +54,6 @@ rightSteering = df_log.Steering - correction
 df_new['img_path'] = pd.concat([df_log.Center_Image, df_log.Left_Image, df_log.Right_Image], ignore_index=True)
 df_new['steering'] = pd.concat([df_log.Steering, leftSteering, rightSteering], ignore_index=True)
 
-images = []
-measurements = []
-for datapoint in range(df_log.shape[0]):
-#for datapoint in range(2881):
-    imgName = df_log.Center_Image[datapoint].split('\\')[-1]
-    imgPath = 'data\\IMG\\' + imgName
-    images.append(cv2.imread(imgPath))
-    measurements.append(df_log.Steering[datapoint])
-
-    imgName = df_log.Left_Image[datapoint].split('\\')[-1]
-    imgPath = 'data\\IMG\\' + imgName
-    images.append(cv2.imread(imgPath))
-    measurements.append(df_log.Steering[datapoint] + correction)
-
-    imgName = df_log.Right_Image[datapoint].split('\\')[-1]
-    imgPath = 'data\\IMG\\' + imgName
-    images.append(cv2.imread(imgPath))
-    measurements.append(df_log.Steering[datapoint]-correction)
-
-augmented_images, augmented_measurements = [], []
-for image,measurement in zip(images,measurements):
-    augmented_images.append(image)
-    augmented_measurements.append(measurement)
-    augmented_images.append(cv2.flip(image,1))
-    augmented_measurements.append(measurement*-1.0)
-
-X_train = np.array(augmented_images)
-y_train = np.array(augmented_measurements)
-
 # Import Keras libraries
 from keras.models import Model, Sequential
 from keras.layers import Flatten, Dense, Lambda, Cropping2D, Dropout
@@ -103,14 +61,14 @@ from keras.layers.convolutional import Convolution2D
 from keras.layers.pooling import MaxPooling2D
 
 # set batch size
-# batch_size = 32
+batch_size = 32
 
-# from sklearn.model_selection import train_test_split
-# train_samples, validation_samples = train_test_split(df_log, test_size=0.2)
+from sklearn.model_selection import train_test_split
+train_samples, validation_samples = train_test_split(df_new, test_size=0.2)
 
 # compile and train the model using the generator function
-# train_generator = generator(train_samples, batch_size=batch_size)
-# validation_generator = generator(validation_samples, batch_size=batch_size)
+train_generator = generator(train_samples, batch_size=batch_size)
+validation_generator = generator(validation_samples, batch_size=batch_size)
 
 model = Sequential()
 model.add(Lambda(lambda x: ((x/255.0) - 0.5), input_shape=(160,320,3)))
@@ -129,14 +87,14 @@ model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
 
-history_object = model.fit(X_train, y_train, validation_split = 0.2, shuffle = True, epochs = 5)
+#history_object = model.fit(X_train, y_train, validation_split = 0.2, shuffle = True, epochs = 5)
                            
-# history_object = model.fit(x=train_generator, \
-#                            validation_data=validation_generator, \
-#                            epochs=5, \
-#                            verbose=1, \
-#                            validation_steps=ceil(len(validation_samples)/batch_size), \
-#                            steps_per_epoch=ceil(len(train_samples)/batch_size))
+history_object = model.fit(x=train_generator, \
+                           validation_data=validation_generator, \
+                           epochs=5, \
+                           verbose=1, \
+                           validation_steps=ceil(len(validation_samples) * 2/batch_size), \
+                           steps_per_epoch=ceil(len(train_samples) * 2/batch_size))
 
 
 model.save('model.h5')
